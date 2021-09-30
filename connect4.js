bb6fdb16e8f5aeecb7ef954e6b06cef737ca3fac
Melissa Nuval <mnuval21@gmail.com>
2021-09-30 01:39:11 -0400

    added some comments

--- connect4.js
+++ connect4.js
@@ -1,58 +1,72 @@
-//table rows and columns
+//Setting the cells 7 wide and 6 high
 const WIDTH = 7;
 const HEIGHT = 6;
 
-let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
+//Active player is set to player 1
+let currPlayer = 1; 
+//Where we will store the array of cells (board[y][x])
+let board = [];
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // set "board" to empty HEIGHT x WIDTH matrix array
+  //set "board" to empty HEIGHT x WIDTH matrix array
   for (let x = 0; x < HEIGHT; x++) {
     board.push(Array.from({ length: WIDTH }));
   }
 }
 
 
+
 function makeHtmlBoard() {
   // get "htmlBoard" variable from the item in HTML w/ID of "board"
-  const board = document.getElementById('board');
+  const htmlBoard = document.getElementById('board');
 
   // create the top of the columns.  Make it clickable.
   const top = document.createElement("tr");
+  // giving the top row an ID of "column-top"
   top.setAttribute("id", "column-top");
+  // listen for clicks on the top row and call the handleClick function
   top.addEventListener("click", handleClick);
 
+  // create 7 <td></td> elements in the top row 
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
+    // give each cell an ID of x
     headCell.setAttribute("id", x);
+    //append each cell to the top row
     top.append(headCell);
   }
-  board.append(top);
+  // append all cells to the board
+  htmlBoard.append(top);
 
-  // main part of the board
+  //  building the main part of the board
+  // create 6 rows
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
 
+    //in each row, create 7 cells with an ID set to {y}-{x}.  The ID will be used to determine if the cell is open or not.
     for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
+      //append each cell into the row
       row.append(cell);
     }
-    board.append(row);
+    //append each row onto the board
+    htmlBoard.append(row);
   }
-  console.log('appending board onto the DOM in makeHtmlBoard()')
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  console.log('finding spot for column x(width) in findSpotForCol(x)')
+  //y=5; while y is greater than or equal to 0; decrease y by 1
   for( let y = HEIGHT - 1; y >= 0; y--){
+    //if there is nothing on the board
     if (!board[y][x]) {
+      //return y
       return y;
     }
   }
@@ -65,11 +79,16 @@ function findSpotForCol(x) {
 function placeInTable(y, x) {
   // make a div and insert into correct table cell
   const piece = document.createElement('div');
+  // give it a class "piece"
   piece.classList.add('piece');
+  // give it a class "p.{whichever current player is set to}" - this will determine the color of our piece.
   piece.classList.add(`p${currPlayer}`);
+  // set top to -50*(y+2);
   piece.style.top = -50 * (y + 2);
 
+  //get the ID of our available cell
   const openSpot = document.getElementById(`${y}-${x}`);
+  //append the piece onto the board
   openSpot.append(piece);
 
 }
@@ -79,8 +98,7 @@ function placeInTable(y, x) {
 function endGame(msg) {
   // pop up alert message
   //alert(msg);
-  //need to pass in (msg) in the modal alert
-  //doesn't work yet
+
   const modal = document.querySelector('.modal');
   const closeButton = document.querySelector('.close-button');
   const message = document.getElementById('msg');
@@ -88,15 +106,6 @@ function endGame(msg) {
   message.innerHTML = (msg);
   modal.classList.add("show-modal");
   
-  //function toggleModal() {
-    //modal.classList.toggle("show-modal");
-  //}
-  //function windowOnClick(event) {
-    //if(event.target === modal){
-      //toggleModal();
-    //}
-  //}
-  //window.addEventListener("click", windowOnClick);
   closeButton.addEventListener("click", function(){
     modal.classList.remove("show-modal");
     window.location.reload();
@@ -108,20 +117,14 @@ function endGame(msg) {
 
 
 
-//trigger.addEventListener("click", toggleModal);
-//closeButton.addEventListener("click", toggleModal);
-//window.addEventListener("click", windowOnClick);
-
-
-
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -173,14 +176,14 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
+  // go through each cell to check if there is piece there
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
